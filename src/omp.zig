607f6a182d84a744f45281591bf615e5f5c69912
sbancuz <sbancuz@protonmail.con>
2024-02-12 10:26:25 +0100

    remove testing code from the library

--- src/omp.zig
+++ src/omp.zig
@@ -144,25 +144,3 @@ pub const omp_ctx = struct {
         kmp.critical_end();
     }
 };
-
-pub fn main() !void {
-    parallel(tes, .{ .string = "gello" }, .{ .num_threads = 8 });
-}
-
-pub fn tes(om: *omp_ctx, args: anytype) void {
-    om.parallel_for(tes2, args, 0, 4, 2, .{});
-}
-
-var a: c_int = 0;
-
-pub fn tes2(om: *omp_ctx, args: anytype) void {
-    _ = args;
-    om.critical(test3, .{});
-    std.debug.print("its aliveeee {}\n", .{a});
-}
-
-pub fn test3(om: *omp_ctx, args: anytype) void {
-    _ = args;
-    _ = om;
-    a += 1;
-}
