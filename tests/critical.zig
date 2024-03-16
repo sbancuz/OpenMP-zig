f89dd0fccfd8e1ec22acec4197a5fa1051d7ffc3
sbancuz <sbancuz@protonmail.com>
2024-03-16 18:08:21 +0100

    Tests: Critical synchronization behaviours

--- tests/critical.zig
+++ tests/critical.zig
@@ -14,7 +14,7 @@ fn parallel_sum(p: *omp.ctx, sum: *u32) void {
     var mysum: u32 = 0;
     p.parallel_for(for_fn, .{&mysum}, @as(u32, 0), @as(u32, 1000), @as(u32, 1), .{});
 
-    p.critical(critical_fn, .{ sum, &mysum });
+    p.critical("none", .none, critical_fn, .{ sum, &mysum });
 }
 
 fn for_fn(p: *omp.ctx, i: u32, mysum: *u32) void {
@@ -32,6 +32,60 @@ test "critical" {
 
     for (params.repetitions) |_| {
         if (!test_omp_critical()) {
+            num_failed += 1;
+        }
+    }
+
+    try std.testing.expect(num_failed == 0);
+}
+
+fn omp_critical_hint(iter: u32) bool {
+    var sum: u32 = 0;
+    const known_sum: u32 = 999 * 1000 / 2;
+
+    omp.parallel(parallel_sum_hint, .{ .shareds = .{ &sum, iter } }, .{});
+
+    return known_sum == sum;
+}
+
+fn parallel_sum_hint(p: *omp.ctx, sum: *u32, iter: u32) void {
+    var mysum: u32 = 0;
+    p.parallel_for(for_fn_hint, .{&mysum}, @as(u32, 0), @as(u32, 1000), @as(u32, 1), .{});
+
+    switch (iter % 4) {
+        0 => {
+            p.critical("a", .uncontended, critical_fn_hint, .{ sum, &mysum });
+        },
+        1 => {
+            p.critical("b", .contended, critical_fn_hint, .{ sum, &mysum });
+        },
+        2 => {
+            p.critical("c", .nonspeculative, critical_fn_hint, .{ sum, &mysum });
+        },
+        3 => {
+            p.critical("d", .speculative, critical_fn_hint, .{ sum, &mysum });
+        },
+        else => {
+            unreachable;
+        },
+    }
+}
+
+fn for_fn_hint(p: *omp.ctx, i: u32, mysum: *u32) void {
+    _ = p;
+    mysum.* = mysum.* + i;
+}
+
+fn critical_fn_hint(p: *omp.ctx, sum: *u32, mysum: *u32) void {
+    _ = p;
+    sum.* += mysum.*;
+}
+
+test "critical_hint" {
+    var num_failed: u32 = 0;
+
+    for (0..params.repetitions) |i| {
+        if (!omp_critical_hint(@intCast(i))) {
             num_failed += 1;
         }
     }
