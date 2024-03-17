48cc63007b90b3419a684fe798a36272e40a9bbc
sbancuz <sbancuz@protonmail.com>
2024-03-17 11:54:38 +0100

    Tests: Parallel if

--- tests/parallel.zig
+++ tests/parallel.zig
@@ -37,10 +37,45 @@ fn critical_para(p: *omp.ctx, sum: *u32, mysum: u32) void {
 
 test "parallel_default" {
     var num_failed: u32 = 0;
-    omp.set_dynamic(false);
-    omp.set_num_threads(4);
     for (0..params.repetitions) |_| {
         if (!try test_omp_parallel_default()) {
+            num_failed += 1;
+        }
+    }
+
+    try std.testing.expect(num_failed == 0);
+}
+
+fn test_omp_parallel_if() !bool {
+    var sum: u32 = 0;
+    var mysum: u32 = 0;
+    const control: u32 = 1;
+    const known_sum: u32 = (params.loop_count * (params.loop_count + 1)) / 2;
+
+    omp.parallel(parallel_if_para, .{ .shareds = .{&sum}, .privates = .{&mysum} }, .{ .condition = control == 0 });
+
+    if (known_sum != sum) {
+        std.debug.print("KNOWN_SUM = {}\n", .{known_sum});
+        std.debug.print("SUM = {}\n", .{sum});
+    }
+
+    try std.testing.expect(mysum == 0);
+
+    return known_sum == sum;
+}
+
+fn parallel_if_para(p: *omp.ctx, sum: *u32, mysum: *u32) void {
+    for (0..params.loop_count + 1) |i| {
+        mysum.* += @as(u32, @intCast(i));
+    }
+
+    p.critical("para", .none, critical_para, .{ sum, mysum.* });
+}
+
+test "parallel_if" {
+    var num_failed: u32 = 0;
+    for (0..params.repetitions) |_| {
+        if (!try test_omp_parallel_if()) {
             num_failed += 1;
         }
     }
