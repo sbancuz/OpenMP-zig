d46e5856b0a1237c59e1e7fca7095dfdcea04f65
sbancuz <sbancuz@protonmail.com>
2024-05-05 17:51:51 +0200

    Test: fix flush sometimes failing

--- tests/params.zig
+++ tests/params.zig
@@ -1,7 +1,7 @@
 pub const loop_count: usize = 1000;
 pub const repetitions: usize = 10;
 
-pub const sleep_time: usize = 1;
+pub const sleep_time: usize = 1_000_000_000;
 
 pub const num_tasks: usize = 25;
 pub const max_tasks_per_thread: usize = 5;
